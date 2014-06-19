d8328cf31a38f6853b588a4ff07944e4665ee1d3
Alric <alric.c.siu@gmail.com>
2014-06-18 17:09:45 -0700

    Add List Head

---
+++ Final Project/ListHead.cpp
@@ -0,0 +1,9 @@
+//
+//  ListHead.cpp
+//  Final Project
+//
+//  Created by Siuperman on 6/18/14.
+//  Copyright (c) 2014 Siuperman. All rights reserved.
+//
+
+#include "ListHead.h"
---
+++ Final Project/ListHead.h
@@ -0,0 +1,42 @@
+//
+//  ListHead.h
+//  Final Project
+//
+//  Created by Siuperman on 6/18/14.
+//  Copyright (c) 2014 Siuperman. All rights reserved.
+//
+
+#ifndef __Final_Project__ListHead__
+#define __Final_Project__ListHead__
+
+#include <iostream>
+#include "App.h"
+#include "BST.h"
+#include "Hash.h"
+
+const int BUCKET_SIZE = 3;
+
+class ListHead
+{
+private:
+    
+    int count;
+    int arySize;
+    int bucketSize = BUCKET_SIZE;
+    
+    Hash pHash;
+    BST pTree;
+    
+public:
+    //Constructor
+    ListHead();
+    ~ListHead();
+    
+    //Member Functions
+    
+    
+};
+
+
+
+#endif /* defined(__Final_Project__ListHead__) */
